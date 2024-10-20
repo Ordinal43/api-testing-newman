f66104e1283a0465cb45a0c628dff24833c04235
Bryan Ramaputra <bryan@stockbit.com>
2024-10-20 10:35:47 -0700

    Add postman testing

---
+++ src/run-test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eu pipefail
+
+echo "Attempting to connect to postman-tests-api"
+until $(nc -zv postman-tests-api 5050); do
+    printf '.'
+    sleep 5
+done
+echo "Connected to postman-tests-api!"
+
+newman run /src/postman_collection.json -e /src/postman_environment.json
+
+exit 0
