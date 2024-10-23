c5cb72338ff53239f9015c96900644255b0c2b7b
Bryan Ramaputra <bryan@stockbit.com>
2024-10-23 13:38:24 -0700

    Fix newman command

--- src/run-test.sh
+++ src/run-test.sh
@@ -9,6 +9,6 @@ until $(nc -zv postman-tests-api 5050); do
 done
 echo "Connected to postman-tests-api!"
 
-newman run /src/postman_collection.json -e /src/postman_environment.json -r cli,htmlextra newman run collection.json -r htmlextra --reporter-htmlextra-export /app/report.html
+newman run /src/postman_collection.json -e /src/postman_environment.json -r cli,htmlextra --reporter-htmlextra-export /app/report.html
 
 exit 0
